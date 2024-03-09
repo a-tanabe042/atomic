087487a6ee8bc816cd840b50e97882ef33c6fc99
uneincrea <a.nojiri.tact@gmail.com>
2024-03-09 22:22:46 +0900

    fix

--- salto_atoms/src/containers/SidebarSubmenu.js
+++ salto_atoms/src/containers/SidebarSubmenu.js
@@ -1,23 +1,40 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 
 function SidebarSubmenu({ submenu, icon }) {
   const location = useLocation();
-  // ドロップダウンの開閉状態を管理するstate。初期値は非表示(false)。
   const [isExpanded, setIsExpanded] = useState(false);
+  // ドロップダウンメニューの参照を保持するための ref
+  const dropdownRef = useRef(null);
 
-  // ドロップダウンを開閉する関数。イベントの伝播を停止して、状態を切り替える。
+  // ドロップダウンメニュー外のクリックを検出するためのイベントリスナーを追加
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsExpanded(false); // メニューの外側がクリックされたら非表示にする
+      }
+    };
+
+    // イベントリスナーをドキュメントに追加
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      // コンポーネントがアンマウントされる時にイベントリスナーを削除
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   const toggleSubmenu = (e) => {
     e.stopPropagation(); // イベントの伝播を停止
-    setIsExpanded(!isExpanded); // 状態を切り替える
+    setIsExpanded(!isExpanded); // ドロップダウンメニューの表示状態を切り替え
   };
 
-  // ドキュメント全体のクリックを監視してドロップダウンを閉じるロジックを削除します。
-  // ユーザーの操作によってのみドロップダウンの状態が変更されるようにするためです。
-
   return (
-    <div className="flex flex-col">
-      <div className="w-full p-2 rounded-full transition duration-300 shadow-md flex items-center justify-center cursor-pointer" onClick={toggleSubmenu}>
+    <div className="flex flex-col" ref={dropdownRef}>
+      <div
+        className="w-full p-2 rounded-full transition duration-300 shadow-md flex items-center justify-center cursor-pointer"
+        onClick={toggleSubmenu}
+      >
         <div className="w-12 h-12 rounded-full flex items-center justify-center">
           {icon}
         </div>
