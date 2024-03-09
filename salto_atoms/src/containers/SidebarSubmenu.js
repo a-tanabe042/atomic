0699251c9944248ad4f2442fcdb56806f8433e9f
uneincrea <a.nojiri.tact@gmail.com>
2024-03-09 22:16:45 +0900

    fix

--- salto_atoms/src/containers/SidebarSubmenu.js
+++ salto_atoms/src/containers/SidebarSubmenu.js
@@ -1,49 +1,33 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 
 function SidebarSubmenu({ submenu, icon }) {
   const location = useLocation();
-  const [isExpanded2, setIsExpanded2] = useState(false);
-  const submenuRef = useRef(null); // Add a ref to the submenu
+  // ドロップダウンの開閉状態を管理するstate。初期値は非表示(false)。
+  const [isExpanded, setIsExpanded] = useState(false);
 
-  useEffect(() => {
-    setIsExpanded2(submenu.some((m) => m.path === location.pathname));
-  }, [location.pathname, submenu]);
-
-  useEffect(() => {
-    const handleDocumentClick = (event) => {
-      // Only toggle if click is outside of the component
-      if (submenuRef.current && !submenuRef.current.contains(event.target)) {
-        setIsExpanded2(false);
-      }
-    };
-
-    // Event listener for clicks outside the component
-    document.addEventListener("mousedown", handleDocumentClick);
-
-    return () => {
-      document.removeEventListener("mousedown", handleDocumentClick);
-    };
-  }, []);
-
+  // ドロップダウンを開閉する関数。イベントの伝播を停止して、状態を切り替える。
   const toggleSubmenu = (e) => {
-    setIsExpanded2(!isExpanded2);
-    e.stopPropagation(); // Prevent the event from propagating to the document
+    e.stopPropagation(); // イベントの伝播を停止
+    setIsExpanded(!isExpanded); // 状態を切り替える
   };
 
+  // ドキュメント全体のクリックを監視してドロップダウンを閉じるロジックを削除します。
+  // ユーザーの操作によってのみドロップダウンの状態が変更されるようにするためです。
+
   return (
-    <div className="flex flex-col" ref={submenuRef}> {/* Apply ref here */}
+    <div className="flex flex-col">
       <div className="w-full p-2 rounded-full transition duration-300 shadow-md flex items-center justify-center cursor-pointer" onClick={toggleSubmenu}>
         <div className="w-12 h-12 rounded-full flex items-center justify-center">
           {icon}
         </div>
       </div>
 
-      {isExpanded2 && (
-       <ul
-         tabIndex={0}
-         className="menu menu-compact dropdown-content p-2 shadow bg-base-100 rounded-box w-52 absolute z-10 right-0 mr-10 mt-20"
-       >
+      {isExpanded && (
+        <ul
+          tabIndex={0}
+          className="menu menu-compact dropdown-content p-2 shadow bg-base-100 rounded-box w-52 absolute z-10 right-0 mr-10 mt-20"
+        >
           {submenu.map((m, index) => (
             <li key={index} className="justify-between mb-1">
               <Link
