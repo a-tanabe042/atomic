c6477335930944dd755761e962dd609766b14ebb
uneincrea <a.nojiri.tact@gmail.com>
2024-03-20 16:03:50 +0900

    fix

--- salto_atoms/src/components/SquareBackgroundAnimation.tsx
+++ salto_atoms/src/components/SquareBackgroundAnimation.tsx
@@ -1,6 +1,13 @@
-import { AnimatePresence, motion } from 'framer-motion'
+import React, { ReactNode } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
 
-const squareItem = [
+interface SquareItem {
+  height: string;
+  left: string;
+  width: string;
+}
+
+const squareItem: SquareItem[] = [
   { height: '8rem', left: '2%', width: '8rem' },
   { height: '4rem', left: '25%', width: '4rem' },
   { height: '5rem', left: '46%', width: '5rem' },
@@ -11,14 +18,18 @@ const squareItem = [
   { height: '5rem', left: '82%', width: '5rem' },
   { height: '3rem', left: '74%', width: '3rem' },
   { height: '8rem', left: '86%', width: '8rem' },
-]
+];
 
-export default function SquareBackgroundAnimation({ children }) {
+interface SquareBackgroundAnimationProps {
+  children: ReactNode;
+}
+
+const SquareBackgroundAnimation: React.FC<SquareBackgroundAnimationProps> = ({ children }) => {
   return (
     <>
       <div className="sticky top-0 z-[-20] h-screen w-full overflow-hidden">
         <AnimatePresence>
-          {squareItem.map((css, idx) => (
+          {squareItem.map((item, idx) => (
             <div key={idx}>
               <motion.div
                 animate="anim"
@@ -29,24 +40,23 @@ export default function SquareBackgroundAnimation({ children }) {
                   bottom: '-10rem',
                   display: 'block',
                   position: 'absolute',
-                  ...css,
+                  ...item,
                 }}
                 variants={{
                   anim: {
-                    opacity: [0, 1, 1, 1, 1, 1, 1, 0], // このままで問題ありません
-                    rotate: 720, // 数値のままで問題ありません
+                    opacity: [0, 1, 1, 1, 1, 1, 1, 0],
+                    rotate: 720,
                     transition: {
                       duration: 15 + Math.floor(Math.random() * 15),
                       ease: 'linear',
                     },
-                    y: `-130vh`, // `-130vh` は文字列ですが、他のキーフレームと型を一致させる
+                    y: `-130vh`,
                   },
                   init: {
-                    opacity: 0, // 数値
-                    y: `0vh`, // 型を一致させるために文字列に変更
+                    opacity: 0,
+                    y: `0vh`,
                   },
                 }}
-                
               />
               <motion.div
                 animate="anim"
@@ -57,7 +67,7 @@ export default function SquareBackgroundAnimation({ children }) {
                   bottom: '-10rem',
                   display: 'block',
                   position: 'absolute',
-                  ...css,
+                  ...item,
                 }}
                 variants={{
                   anim: {
@@ -81,5 +91,7 @@ export default function SquareBackgroundAnimation({ children }) {
       </div>
       <div style={{ marginTop: 'calc(100vh / -1)' }}>{children}</div>
     </>
-  )
-}
+  );
+};
+
+export default SquareBackgroundAnimation;
--- salto_atoms/src/containers/LeftSidebar.js
+++ salto_atoms/src/containers/LeftSidebar.js
@@ -1,15 +1,10 @@
-// LeftSidebar コンポーネント
-import React from 'react';
-import { useRecoilValue } from 'recoil';
-import { sectionState } from '../state'; // 適切なパスに修正してください
 import SectionNav from "../components/nav/SectionNav";
 
 function LeftSidebar() {
-  const sections = useRecoilValue(sectionState);
 
   return (
     <div className="w-40 bg-base-100 my-8 shadow-xl rounded-2xl opacity-80">
-      <SectionNav sections={sections} />
+      <SectionNav  />
     </div>
   );
 }
