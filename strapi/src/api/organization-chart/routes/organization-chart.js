e0cad76198ae1b83c5c35b6e6f716964292d1602
uneincrea <a.nojiri.tact@gmail.com>
2024-03-23 17:46:21 +0900

    commit

--- strapi/src/api/organization-chart/routes/organization-chart.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * organization-chart router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::organization-chart.organization-chart');
--- strapi/src/api/organization-chart/services/organization-chart.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * organization-chart service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::organization-chart.organization-chart');
